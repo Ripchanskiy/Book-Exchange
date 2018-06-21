f7839234d429227c17f8300a42b64951671c8ced
Sean Leamy <stleamy@gmail.com>
2018-06-21 11:23:10 +1000

    Updated routes to interact with database

--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/book');
 
+// Get all books
 router.get('/', (req, res, next) => {
-    res.send('Get all books');
+    Book.find({}, (error, books) => {
+        if(error) {
+            res.send('Error');
+        } else { 
+            res.json(books);
+        }
+    });
 });
 
+// Get single book by ID
 router.get('/:id', (req, res, next) => {
-    res.send('Get one book');
+    Book.findById(req.params.id, (error, book) => {
+        if(error) { 
+            res.json({success: false, message: 'Could not find book'});
+        } else {
+            res.json(book);
+        }
+    })
 });
 
+// Add new book
 router.post('/add', (req, res, next) => {
     
+    let newBook = new Book({
+        title: req.body.title,
+        author: req.body.author
+    });
+
+    newBook.save(error => {
+        if(error) {
+            res.json({success: false, message: 'Failed to add book'});
+        } else {
+            res.json({success: true, message: 'Book added'})
+        }
+    });
 })
 
+// Delete book
 router.delete('/:id', (req, res, next) => {
 
 });
