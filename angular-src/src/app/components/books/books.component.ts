95a6bfebd4227ffe10bc22021e7a842e36bb0c10
Sean Leamy <stleamy@gmail.com>
2018-07-13 15:05:42 +1000

    Added Material Icons and updated books page

--- angular-src/src/app/components/books/books.component.ts
+++ angular-src/src/app/components/books/books.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { Observable } from 'rxjs';
+import { categories } from '../../categories';
 
 @Component({
     selector: 'app-books',
@@ -10,12 +11,15 @@ import { Observable } from 'rxjs';
 export class BooksComponent implements OnInit {
 
     books: Object;
+    categories: string[];
 
     constructor(private bookService: BookService) { }
 
     ngOnInit() {
 
+        this.categories = categories;
         this.bookService.getBooks().subscribe(books => this.books = books);
+        //this.bookService.getBooks().subscribe((books: any) => console.log(books[1].seller));
     }
 
 
